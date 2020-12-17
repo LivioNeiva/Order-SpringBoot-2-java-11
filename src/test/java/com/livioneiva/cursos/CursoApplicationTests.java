1c5d1047b7c1d715326f74e93188380847fff2e8
LivioNeiva <livio_neiva@hotmail.com>
2020-12-17 19:37:06 -0300

    Project created

---
+++ src/test/java/com/livioneiva/cursos/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livioneiva.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
